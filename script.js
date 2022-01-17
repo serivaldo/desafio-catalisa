2633960a9287eaa623a315611950d4f6741ae0c8
serivaldo <seriapdasilva@gmail.com>
2022-01-16 22:32:28 -0300

    Implementar personagem aleatorio

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
 
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 876);
+}
 
 pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
-}).then((response) => response.jason()).then((data)) => {
+}).then((response) => response.json()).then((data) => {
     imagem.src = data.image;
     imagem.alt= data.name;
     nomeDoPersonagem.innerHTML = data.name;
